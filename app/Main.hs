911903aabd43e80bb0c11f2a4de6547be76161ce
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 14:01:05 +0300

    draft

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
+import Data.Bits (Bits(xor))
+
+p1Board :: [Int]
+p1Board = [4,4,4,4,4,4]
+p2Board :: [Int]
+p2Board = [4,4,4,4,4,4]
+p1Box :: Int
+p1Box = 0
+p2Box :: Int
+p2Box = 0
+
+game :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box p2Box, whose turn
+
+distStonesP1 :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box, stoneCount, index
+distStonesP1 p1Board p2Board p1Box x
+    |x == 0 = peekLastStone 
+    |p1Board !! index == 1
 
 main :: IO ()
 main = do 
