af6aff09bd18ff22ba360ebf338538c6acb87df7
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 13:24:38 +0300

    ideas

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell :)BbB!"
+main = do 
+    putStrLn "Hello, Haskell :)BbB!"
+    putStrLn "Hello, Haskell :)BbB!"
+-- dist stones (player1 and 2 seperately)
+    -- peekLastStone, displayBoard
+-- a logic to keep turns
+-- isGameOver check both players' holes
+-- emptyBoard (isgameover calls it in case of a winner)
