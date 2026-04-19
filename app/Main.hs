80417ce152509aca8496dc625873781574951d02
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 17:08:35 +0300

    Update Main.hs
    Co-Authored-By: AlprTm <275313955+AlprTm@users.noreply.github.com>
    Co-Authored-By: AyseSerife <208360306+AyseSerife@users.noreply.github.com>

--- app/Main.hs
+++ app/Main.hs
@@ -19,39 +19,56 @@ game p1board p2board p1box p2Box whoseTurn = do
 -- first things first
     if whoseTurn == 1 
         then 
-            if peekPlayersBoard (hole, p1board) == 1
+            if peekPlayersBoard (index, p1board) == 1
                 then handle1StoneCase -- TODO function
-                else game (tupleAdjuster1 (distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole whoseTurn) p2Box)
+                else game (tupleAdjuster1 (distStonesP1 p1Board p2Board p1Box (peekPlayersBoard index p1board) hole whoseTurn) p2Box)
         else 
-            if peekPlayersBoard (hole, p2board) == 1
+            if peekPlayersBoard (index, p2board) == 1
                 then handle1StoneCase -- TODO function
-                else game (tupleAdjuster2 (distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole whoseTurn) p1Box)
+                else game (tupleAdjuster2 (distStonesP2 p2Board p1Board p2Box (peekPlayersBoard index p2board) hole whoseTurn) p1Box)
+    where 
+        index = hole - 1
     
 peekPlayersBoard :: (Int, [Int]) -> Int
-peekPlayersBoard (hole, board) = board !! (hole + 1)
+peekPlayersBoard (index, board) = board !! index
 
-tupleAdjuster1 :: ([Int], [Int], Int, Int, Int, Int) -> Int -> ([Int], [Int], Int, Int, Int)
+tupleAdjuster1 :: ([Int], [Int], Int, Int, Int, Int) -> Int -> ([Int], [Int], Int, Int, Int) -- stonecounter index
 tupleAdjuster1 (p1Board, p2Board, p1Box, _, _, whoseTurn) p2Box = (p1Board, p2Board, p1Box, p2Box, whoseTurn)
-
 
 tupleAdjuster2 :: ([Int], [Int], Int, Int, Int, Int) -> Int -> ([Int], [Int], Int, Int, Int)
 tupleAdjuster2 (p2Board, p1Board, p2Box, _, _, whoseTurn) p1Box = (p1Board, p2Board, p1Box, p2Box, whoseTurn)
 
 distStonesP1 :: ([Int], [Int], Int, Int, Int, Int) -> ([Int], [Int], Int ,Int, Int, Int) --p1board p2board p1box, stoneCount, index, whoseTurn
 distStonesP1 (p1Board, p2Board, p1Box, stoneCount, index)
-    = distribute (p1Board ++ p1Box ++ p2Board, stoneCount, index)
+    = distribute ((take index p1Board ++ [1] ++ drop (index + 1) p1Board) ++ p1Box ++ p2Board, stoneCount, index)
 
 distStonesP2 :: ([Int], [Int], Int, Int, Int, Int) -> ([Int], [Int], Int ,Int, Int, Int) --p2board p1board p2box, stoneCount, index, whoseTurn
 distStonesP2 (p2Board, p1Board, p2Box, stoneCount, index)
-    = distribute (p2Board ++ p2Box ++ p1Board, stoneCount, index)
+    = distribute ((take index p2Board ++ [1] ++ drop (index + 1) p2Board) ++ p2Box ++ p1Board, stoneCount, index)
 
 distribute :: ([Int], Int, Int, Int) -> ([Int], Int, Int, Int)  -- concatenated list, stoneCount, index, whoseTurn
 distribute (list, stoneCount, index)
     | stoneCount == 1 = peekLastStone
-    | otherwise = distribute(take index board ++ [(list !! index) + 1] ++ drop (index + 1) board, stoneCount - 1, index + 1)
+    | otherwise = distribute(take index list ++ [(list !! index) + 1] ++ drop (index + 1) list, stoneCount - 1, index + 1)
         
 peekLastStone :: ([Int], Int, Int) -> ([Int], Int, Int) -- concat list, index, whoseTurn
-peekLastStone 
+peekLastStone (list, index, whoseTurn) 
+    | index < 6 = if list !! index == 0 -- our last piece came to an empty hole
+        then if b  == 0 -- opposing hole is also empty
+            then (take index list ++ [1] ++ drop (index + 1) list, index, d) 
+            else (take 6 list ++ [a + b] ++ take (5 - index) c ++ [0] ++ drop (6 - index) c, index, d) -- opposing hole isn't empty
+        else (take index list ++ [a + 1] ++ drop (index + 1) list, index, d) -- OUR hole wasn't empty anyway
+    | index == 6 = (take 6 list ++ [a + b] ++ c, index, e)
+    | otherwise = (take index list ++ [f + 1] ++ drop (index + 1) list, index, d) 
+
+    where 
+        a = list !! 6 -- current box #stones
+        b = list !! (12 - index) -- opposing box #stones
+        c :: [Int]
+        c = drop 7 list
+        d = if whoseTurn == 1 then 2 else 1 
+        e = if whoseTurn == 1 then 1 else 2 
+        f = list !! index
 
 handle1StoneCase 
 main :: IO ()
