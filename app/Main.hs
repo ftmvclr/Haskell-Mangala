0a3fbe1b77f13a4a03b772a28fd98ff9995b2fee
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 14:40:13 +0300

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -18,16 +18,24 @@ game p1board p2board p1box p2Box whoseTurn = do
     putStrLn ("You selected hole: " ++ show hole)
 -- first things first
     if whoseTurn == 1 
-        then distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole
-        else distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole
+        then 
+            if (peekPlayersBoard hole p1board) == 1
+                then handleCase1 -- TODO function
+                else distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole
+        else 
+            if (peekPlayersBoard hole p2board) == 1
+                then handleCase1 -- TODO function
+                else distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole
 
 distStonesP1 :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box, stoneCount, index
 distStonesP1 p1Board p2Board p1Box x
     |x == 0 = peekLastStone
-    |
+    |otherwise = distribute 
 
 peekPlayersBoard :: Int -> [Int] -> Int-- index, board
+    peekPlayersBoard index board = board !! (index+1)
 
+distribute :: ([Int], Int, Int) -> ([Int], Int, Int)  -- concatenated list, stonecount index
 main :: IO ()
 main = putStrLn "Hello, Haskell :)BbB!"
 -- dist stones (player1 and 2 seperately)
