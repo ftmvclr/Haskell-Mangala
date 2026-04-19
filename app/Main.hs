c2e80c09835a0441ffaa2688cf6c128590323b17
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 15:55:45 +0300

    Update Main.hs
    Co-Authored-By: AlprTm <275313955+AlprTm@users.noreply.github.com>
    Co-Authored-By: AyseSerife <208360306+AyseSerife@users.noreply.github.com>

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ p1Box = 0
 p2Box :: Int
 p2Box = 0
 
-game :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box p2Box, whose turn
+game :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box p2Box, whoseTurn
 game p1board p2board p1box p2Box whoseTurn = do
     putStrLn $"Player {whoseTurn} enter hole number (1-6):"
     input <- getLine 
@@ -19,30 +19,41 @@ game p1board p2board p1box p2Box whoseTurn = do
 -- first things first
     if whoseTurn == 1 
         then 
-            if (peekPlayersBoard (hole, p1board)) == 1
-                then handleCase1 -- TODO function
-                else distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole
+            if peekPlayersBoard (hole, p1board) == 1
+                then handle1StoneCase -- TODO function
+                else game (tupleAdjuster1 (distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole whoseTurn) p2Box)
         else 
-            if (peekPlayersBoard (hole, p1board)) == 1
-                then handleCase1 -- TODO function
-                else distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole
+            if peekPlayersBoard (hole, p2board) == 1
+                then handle1StoneCase -- TODO function
+                else game (tupleAdjuster2 (distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole whoseTurn) p1Box)
     
+peekPlayersBoard :: (Int, [Int]) -> Int
+peekPlayersBoard (hole, board) = board !! (hole + 1)
 
-distStonesP1 :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int,) --p1board p2board p1box, stoneCount, index
+tupleAdjuster1 :: ([Int], [Int], Int, Int, Int, Int) -> Int -> ([Int], [Int], Int, Int, Int)
+tupleAdjuster1 (p1Board, p2Board, p1Box, _, _, whoseTurn) p2Box = (p1Board, p2Board, p1Box, p2Box, whoseTurn)
+
+
+tupleAdjuster2 :: ([Int], [Int], Int, Int, Int, Int) -> Int -> ([Int], [Int], Int, Int, Int)
+tupleAdjuster2 (p2Board, p1Board, p2Box, _, _, whoseTurn) p1Box = (p1Board, p2Board, p1Box, p2Box, whoseTurn)
+
+distStonesP1 :: ([Int], [Int], Int, Int, Int, Int) -> ([Int], [Int], Int ,Int, Int, Int) --p1board p2board p1box, stoneCount, index, whoseTurn
 distStonesP1 (p1Board, p2Board, p1Box, stoneCount, index)
-    |stoneCount == 0 = peekLastStone
-    |otherwise = distribute (p1Board ++ p1Box ++ p2Board, stoneCount, index)
+    = distribute (p1Board ++ p1Box ++ p2Board, stoneCount, index)
 
-peekPlayersBoard :: Int -> [Int] -> Int-- index, board
-peekPlayersBoard index board = board !! (index+1)
+distStonesP2 :: ([Int], [Int], Int, Int, Int, Int) -> ([Int], [Int], Int ,Int, Int, Int) --p2board p1board p2box, stoneCount, index, whoseTurn
+distStonesP2 (p2Board, p1Board, p2Box, stoneCount, index)
+    = distribute (p2Board ++ p2Box ++ p1Board, stoneCount, index)
 
-distribute :: ([Int], Int, Int) -> ([Int], Int, Int)  -- concatenated list, stoneCount index
+distribute :: ([Int], Int, Int, Int) -> ([Int], Int, Int, Int)  -- concatenated list, stoneCount, index, whoseTurn
 distribute (list, stoneCount, index)
     | stoneCount == 1 = peekLastStone
     | otherwise = distribute(take index board ++ [(list !! index) + 1] ++ drop (index + 1) board, stoneCount - 1, index + 1)
         
-peekLastStone :: ([Int], Int) -> ([Int], Int)
+peekLastStone :: ([Int], Int, Int) -> ([Int], Int, Int) -- concat list, index, whoseTurn
 peekLastStone 
+
+handle1StoneCase 
 main :: IO ()
 main = putStrLn "Hello, Haskell :)BbB!"
 -- dist stones (player1 and 2 seperately)
