5390fb4030c20dd6fa3b249dba3e12e1d71c9661
ftmvclr <fatimaavcilar@marun.edu.tr>
2026-04-19 14:22:44 +0300

    bitti arkidaşlar

--- app/Main.hs
+++ app/Main.hs
@@ -11,16 +11,25 @@ p2Box :: Int
 p2Box = 0
 
 game :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box p2Box, whose turn
+game p1board p2board p1box p2Box whoseTurn = do
+    putStrLn $"Player {whoseTurn} enter hole number (1-6):"
+    input <- getLine 
+    let hole = read input :: Int 
+    putStrLn ("You selected hole: " ++ show hole)
+-- first things first
+    if whoseTurn == 1 
+        then distStonesP1 p1Board p2Board p1Box (peekPlayersBoard hole p1board) hole
+        else distStonesP2 p2Board p1Board p2Box (peekPlayersBoard hole p2board) hole
 
 distStonesP1 :: ([Int], [Int], Int, Int, Int) -> ([Int], [Int], Int ,Int, Int) --p1board p2board p1box, stoneCount, index
 distStonesP1 p1Board p2Board p1Box x
-    |x == 0 = peekLastStone 
-    |p1Board !! index == 1
+    |x == 0 = peekLastStone
+    |
+
+peekPlayersBoard :: Int -> [Int] -> Int-- index, board
 
 main :: IO ()
-main = do 
-    putStrLn "Hello, Haskell :)BbB!"
-    putStrLn "Hello, Haskell :)BbB!"
+main = putStrLn "Hello, Haskell :)BbB!"
 -- dist stones (player1 and 2 seperately)
     -- peekLastStone, displayBoard
 -- a logic to keep turns
